a95376dcfdf35984357a65b3641adac38833a188
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-03-07 12:50:38 +0100

    Initial clean code
    Signed-off-by: Lorenzo Rocca <lorenzo.rocca.work@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { Geist, Geist_Mono, Outfit } from "next/font/google"
+import { Geist_Mono, Outfit } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import React from "react"
 
 const outfit = Outfit({subsets:['latin'],variable:'--font-sans'})
 
